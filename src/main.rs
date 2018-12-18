162fd25d3e5634fbc9109d2613806ca1c04a56f5
Clifford T. Matthews <ctm@devctm.com>
2018-12-18 14:11:42 -0700

    Includes sample code from https://github.com/geal/nom-trace
    In order to make the sample code work, I needed to use "nom_trace" instead
    of "nom-trace" in the external crate line.  I also needed to use "nom::digit"
    instead of "digit" in the source.
    
    I probably should have committed before making those changes, but oh well.
    
    FWIW, the output I get when I run it is different from the output in the
    nom-trace README.md.  It looks to me like its reporting byte slices instead
    of string slices.  I'm guessing this is because we start out with a
    parser<Vec<&[u8]>>.  I'm going to see if I can get it to work with string
    slices.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate nom;
+#[macro_use] extern crate nom_trace;
+
+//adds a thread local storage object to store the trace
+declare_trace!();
+
+pub fn main() {
+  named!(parser<Vec<&[u8]>>,
+    //wrap a parser with tr!() to add a trace point
+    tr!(preceded!(
+      tr!(tag!("data: ")),
+      tr!(delimited!(
+        tag!("("),
+        separated_list!(
+          tr!(tag!(",")),
+          tr!(nom::digit)
+        ),
+        tr!(tag!(")"))
+      ))
+    ))
+  );
+
+  println!("parsed: {:?}", parser(&b"data: (1,2,3)"[..]));
+
+  // prints the last parser trace
+  print_trace!();
+
+  // the list of trace events can be cleared
+  reset_trace!();
 }
