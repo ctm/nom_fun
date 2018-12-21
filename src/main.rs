2086984985c1b8400d470568e3765b002b1aa4c8
Clifford T. Matthews <ctm@devctm.com>
2018-12-21 09:01:24 -0700

    Fixed clippy warnings.  Removed sample code I was originally playing with.
    main.rs now contains an "all" parser that recognizes the various ways I've
    represented a duration in the my Monday_Intervals log.  It contains some
    tests and is basically ready to be moved into its own module and give
    a better name than all.

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,19 @@
 #[macro_use]
 extern crate nom;
-#[macro_use]
-extern crate nom_trace;
 
 use nom::digit;
 use nom::types::CompleteStr;
 use std::time::Duration;
 
-//adds a thread local storage object to store the trace
-declare_trace!();
-
 pub fn main() {
-    named!(parser<&str, Vec<&str>>,
-      //wrap a parser with tr!() to add a trace point
-      tr!(preceded!(
-        tr!(tag!("data: ")),
-        tr!(delimited!(
-          tag!("("),
-          separated_list!(
-            tr!(tag!(",")),
-            tr!(nom::digit)
-          ),
-          tr!(tag!(")"))
-        ))
-      ))
-    );
-
-    println!("parsed: {:?}", parser(&"data: (1,2,3)"[..]));
-
-    // prints the last parser trace
-    print_trace!();
-
-    // the list of trace events can be cleared
-    reset_trace!();
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Color {
-    pub red: u8,
-    pub green: u8,
-    pub blue: u8,
-}
-
-#[allow(dead_code)]
-fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
-    u8::from_str_radix(input, 16)
-}
-
-#[allow(dead_code)]
-fn is_hex_digit(c: char) -> bool {
-    c.is_digit(16)
-}
-
-named!(hex_primary<&str, u8>,
-  map_res!(take_while_m_n!(2, 2, is_hex_digit), from_hex)
-);
-
-named!(hex_color<&str, Color>,
-  do_parse!(
-           tag!("#")   >>
-    red:   hex_primary >>
-    green: hex_primary >>
-    blue:  hex_primary >>
-    (Color { red, green, blue })
-  )
-);
-
-#[test]
-fn parse_color() {
+    // For now, this is just a call so we don't get complaints about unused
+    // constants.
     assert_eq!(
-        hex_color("#2F14DF"),
-        Ok((
-            "",
-            Color {
-                red: 47,
-                green: 20,
-                blue: 223,
-            }
-        ))
+        Duration::new(
+            2 * SECONDS_IN_HOUR + 25 * SECONDS_IN_MINUTE + 36,
+            7 * TENTHS_IN_NANOSECOND
+        ),
+        all(CompleteStr("2:25:36.7")).unwrap().1
     );
 }
 
@@ -112,30 +48,6 @@ const MINUTES_IN_HOUR: u64 = 60;
 const SECONDS_IN_HOUR: u64 = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
 const NANOSECONDS_IN_SECOND: u32 = 1_000_000_000;
 const TENTHS_IN_NANOSECOND: u32 = NANOSECONDS_IN_SECOND / 10;
-
-// So, nom::digit recognizes one *or more* digits.  I don't yet know how
-// to recognize exactly one digit.  Ugh!
-
-// Heh! earlier I had is_a!("0123456789") until I found digit.  Now, looking
-// through the source I find:
-//
-//     block_named!(digit, is_a!("0123456789"));
-//
-// Since that's the case, I don't feel bad about defining my own single digit,
-// if I don't find something better.
-//
-// Oh, hey, looks like there may be an i64 block that I can use that will
-// scoop up the digits and then do the parsing for me.  I should look to
-// see if there is a u64 or anything similar.  This will require some poking
-// around and likely some experimentation, too.
-//
-// HMMMM... I may have prematurely jumped to conclusions.  I don't know
-// that the above block_named! stuff is actually defining digit.  It could
-// be something else.
-//
-// Looks like this is it:
-//
-//     complete_named!(digit, is_a!("0123456789"));
 
 named!(hour_prefix<CompleteStr, Duration>,
   do_parse!(
@@ -176,7 +88,7 @@ named!(double_digit_minute_prefix<CompleteStr, Duration>,
     tens: zero_through_five >>
     ones: single_digit >>
     tag!(":") >>
-    (Duration::new(((tens as u64 * 10) + ones as u64) * SECONDS_IN_MINUTE, 0))
+    (Duration::new(((u64::from(tens) * 10) + u64::from(ones)) * SECONDS_IN_MINUTE, 0))
   )
 );
 
@@ -192,7 +104,7 @@ named!(single_digit_minute_prefix<CompleteStr, Duration>,
   do_parse!(
     ones: single_digit >>
     tag!(":") >>
-    (Duration::new(ones as u64 * SECONDS_IN_MINUTE, 0))
+    (Duration::new(u64::from(ones) * SECONDS_IN_MINUTE, 0))
   )
 );
 
@@ -200,14 +112,14 @@ named!(double_digit_seconds<CompleteStr, Duration>,
   do_parse!(
     tens: zero_through_five >>
     ones: single_digit >>
-    (Duration::new((tens as u64 * 10) + ones as u64, 0))
+    (Duration::new((u64::from(tens) * 10) + u64::from(ones), 0))
   )
 );
 
 named!(single_digit_seconds<CompleteStr, Duration>,
   do_parse!(
     ones: single_digit >>
-    (Duration::new(ones as u64, 0))
+    (Duration::new(u64::from(ones), 0))
   )
 );
 
@@ -215,7 +127,7 @@ named!(tenths<CompleteStr, Duration>,
   do_parse!(
     tag!(".") >>
     tenth: single_digit >>
-    (Duration::new(0, tenth as u32 * TENTHS_IN_NANOSECOND))
+    (Duration::new(0, u32::from(tenth) * TENTHS_IN_NANOSECOND))
   )
 );
 
@@ -314,6 +226,14 @@ fn test_all() {
     assert_eq!(
         Duration::new(2 * SECONDS_IN_HOUR + 25 * SECONDS_IN_MINUTE + 36, 0),
         all(CompleteStr("2:25:36")).unwrap().1
+    );
+
+    assert_eq!(
+        Duration::new(
+            2 * SECONDS_IN_HOUR + 25 * SECONDS_IN_MINUTE + 36,
+            7 * TENTHS_IN_NANOSECOND
+        ),
+        all(CompleteStr("2:25:36.7")).unwrap().1
     );
 
     assert_eq!(
